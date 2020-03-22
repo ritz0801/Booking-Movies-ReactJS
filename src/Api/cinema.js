b2d35bae72767e56a55aee5ded82f6d5e8aa2d07
Vo Anh Dung <vodung0801@gmail.com>
2020-03-22 18:20:19 +0700

    .

--- src/Api/cinema.js
+++ src/Api/cinema.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://movie0706.cybersoft.edu.vn/api/QuanLyRap/'
+    baseURL: 'https://movie0706.cybersoft.edu.vn/api/QuanLyRap/'
 });
 export default api;
--- src/Api/film.js
+++ src/Api/film.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://movie0706.cybersoft.edu.vn/api/QuanLyPhim/'
+    baseURL: 'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/'
 });
 export default api;
