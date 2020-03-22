690a56ab984362644f8ecabf2af74b80bb140f0c
Vo Anh Dung <vodung0801@gmail.com>
2020-03-22 18:35:07 +0700

    .

--- src/index.js
+++ src/index.js
@@ -13,11 +13,12 @@ import { Provider } from "react-redux";
 import { SnackbarProvider } from "notistack";
 import rootReducer from "./Reducers/rootReducer";
 
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk), devTools
   )
 );
 
