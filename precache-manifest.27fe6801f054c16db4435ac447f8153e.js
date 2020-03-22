5a23dae8245ab3d3daaeab098372d6ec21005735
Vo Anh Dung <vodung0801@gmail.com>
2020-03-22 18:07:53 +0700

    Updates

---
+++ precache-manifest.27fe6801f054c16db4435ac447f8153e.js
@@ -0,0 +1,310 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97e37895455b31939f1f63d1a39909a",
+    "url": "/Booking-Movies-ReactJS/index.html"
+  },
+  {
+    "revision": "fcee2b2d8dbae7867d9a",
+    "url": "/Booking-Movies-ReactJS/static/css/2.1e4af1c3.chunk.css"
+  },
+  {
+    "revision": "13c5a8425a6b8ef350a9",
+    "url": "/Booking-Movies-ReactJS/static/css/main.6af9f30a.chunk.css"
+  },
+  {
+    "revision": "fcee2b2d8dbae7867d9a",
+    "url": "/Booking-Movies-ReactJS/static/js/2.4a42c618.chunk.js"
+  },
+  {
+    "revision": "13c5a8425a6b8ef350a9",
+    "url": "/Booking-Movies-ReactJS/static/js/main.84f3fcad.chunk.js"
+  },
+  {
+    "revision": "5802c90e62fd284e05ff",
+    "url": "/Booking-Movies-ReactJS/static/js/runtime-main.1a8eee83.js"
+  },
+  {
+    "revision": "1c99dfbecc9492120366e799909871f3",
+    "url": "/Booking-Movies-ReactJS/static/media/QR.1c99dfbe.png"
+  },
+  {
+    "revision": "1908ba368e4b609f9527cb02aa7fcb3f",
+    "url": "/Booking-Movies-ReactJS/static/media/acb.1908ba36.png"
+  },
+  {
+    "revision": "3ab1bcc02675c47134c32d046f3525e0",
+    "url": "/Booking-Movies-ReactJS/static/media/ant-man.3ab1bcc0.png"
+  },
+  {
+    "revision": "f3e44d9762fa531f1d8c2e69f0baf430",
+    "url": "/Booking-Movies-ReactJS/static/media/avatar-2-doc.f3e44d97.jpg"
+  },
+  {
+    "revision": "9f6d5a9a5363f1858ff11bcb87fa8f5b",
+    "url": "/Booking-Movies-ReactJS/static/media/avatar2.9f6d5a9a.jpg"
+  },
+  {
+    "revision": "fd7b7df018b72be1970cd82c92747eab",
+    "url": "/Booking-Movies-ReactJS/static/media/avengers-infinity-war-doc.fd7b7df0.png"
+  },
+  {
+    "revision": "8487ffcc5dae28a2730a9625a732f4ad",
+    "url": "/Booking-Movies-ReactJS/static/media/avengers-infinity-war.8487ffcc.png"
+  },
+  {
+    "revision": "5de6f656b882b385e291b50b8ddb7372",
+    "url": "/Booking-Movies-ReactJS/static/media/backapp.5de6f656.jpg"
+  },
+  {
+    "revision": "10701cfcc649350cad9fe5d64252b2a2",
+    "url": "/Booking-Movies-ReactJS/static/media/batman-v-superman-dawn-of-justicess-doc.10701cfc.png"
+  },
+  {
+    "revision": "5f8794e2b809f442c038ae14d5a822f3",
+    "url": "/Booking-Movies-ReactJS/static/media/batman-v-superman-dawn-of-justicess.5f8794e2.png"
+  },
+  {
+    "revision": "8ad9f57b133f90746bdf1b4b1ce9dffa",
+    "url": "/Booking-Movies-ReactJS/static/media/bidvbank.8ad9f57b.png"
+  },
+  {
+    "revision": "56ea8576e4d16eb121dffa6772d8f959",
+    "url": "/Booking-Movies-ReactJS/static/media/cinema.56ea8576.png"
+  },
+  {
+    "revision": "b2039e87cb4a699700377dd245709317",
+    "url": "/Booking-Movies-ReactJS/static/media/cinema1.b2039e87.png"
+  },
+  {
+    "revision": "29eb1835feaa752d6eab1d4c9586b116",
+    "url": "/Booking-Movies-ReactJS/static/media/eximbank.29eb1835.png"
+  },
+  {
+    "revision": "43b789c9e398153d7e385154ef4aa526",
+    "url": "/Booking-Movies-ReactJS/static/media/fantastic-four.43b789c9.png"
+  },
+  {
+    "revision": "0eb5f15e770b0a8913f577c9f3cee577",
+    "url": "/Booking-Movies-ReactJS/static/media/fb.0eb5f15e.svg"
+  },
+  {
+    "revision": "45dff0bab1cfddd025515bb1c940497d",
+    "url": "/Booking-Movies-ReactJS/static/media/gg.45dff0ba.svg"
+  },
+  {
+    "revision": "03b2f52bff03068a460adefd4a189963",
+    "url": "/Booking-Movies-ReactJS/static/media/gpbank.03b2f52b.png"
+  },
+  {
+    "revision": "2224c0c4bed1509e092df30b7f0bbf1f",
+    "url": "/Booking-Movies-ReactJS/static/media/home.2224c0c4.png"
+  },
+  {
+    "revision": "a176e1c9f4b5dac9c5ad4c2744ed6f2e",
+    "url": "/Booking-Movies-ReactJS/static/media/inside-out.a176e1c9.png"
+  },
+  {
+    "revision": "e910092a4caee20e3cf71246b39d0248",
+    "url": "/Booking-Movies-ReactJS/static/media/jurassic-world.e910092a.png"
+  },
+  {
+    "revision": "b7ba04ebb170c6455e39eff3f54ffcd9",
+    "url": "/Booking-Movies-ReactJS/static/media/kienlongbank.b7ba04eb.png"
+  },
+  {
+    "revision": "f1d17422f8d8fcbc2f67188cf7ac0051",
+    "url": "/Booking-Movies-ReactJS/static/media/lienvietbank.f1d17422.png"
+  },
+  {
+    "revision": "184067dcffebc899e4591756ee08fa58",
+    "url": "/Booking-Movies-ReactJS/static/media/lotte.184067dc.png"
+  },
+  {
+    "revision": "1ae91b76bc7d246ea01bfa959024ac4c",
+    "url": "/Booking-Movies-ReactJS/static/media/mad-max-fury-road.1ae91b76.png"
+  },
+  {
+    "revision": "dbdfa72ddf56b97cf71c90d311d79827",
+    "url": "/Booking-Movies-ReactJS/static/media/mb.dbdfa72d.png"
+  },
+  {
+    "revision": "4d84bc851ee183b84029deed192506f6",
+    "url": "/Booking-Movies-ReactJS/static/media/namabank.4d84bc85.png"
+  },
+  {
+    "revision": "b90ce8854df098544b5e8e037f9363d5",
+    "url": "/Booking-Movies-ReactJS/static/media/publicbank.b90ce885.png"
+  },
+  {
+    "revision": "bb255193bf43a5527e2f60a89a389b5a",
+    "url": "/Booking-Movies-ReactJS/static/media/pvcombank.bb255193.png"
+  },
+  {
+    "revision": "9406e50a4afdcc92e21fb45da866952e",
+    "url": "/Booking-Movies-ReactJS/static/media/sacombank.9406e50a.png"
+  },
+  {
+    "revision": "ec4d02b9f6fe635f14f840ae0536f7d1",
+    "url": "/Booking-Movies-ReactJS/static/media/scbbank.ec4d02b9.png"
+  },
+  {
+    "revision": "45ce994ee307618da7ee3799b309999e",
+    "url": "/Booking-Movies-ReactJS/static/media/shb.45ce994e.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Booking-Movies-ReactJS/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Booking-Movies-ReactJS/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Booking-Movies-ReactJS/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Booking-Movies-ReactJS/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "31d8c4486cd3057eac7ba77f820fc8d3",
+    "url": "/Booking-Movies-ReactJS/static/media/slide0.31d8c448.png"
+  },
+  {
+    "revision": "97d1882c2b8809777fca572c979a49dc",
+    "url": "/Booking-Movies-ReactJS/static/media/slide1.97d1882c.png"
+  },
+  {
+    "revision": "efe3e36fb55ff580b44d001829441306",
+    "url": "/Booking-Movies-ReactJS/static/media/slide2.efe3e36f.png"
+  },
+  {
+    "revision": "994897fe89bb7fc66bf66800e6fb1b79",
+    "url": "/Booking-Movies-ReactJS/static/media/slide4.994897fe.png"
+  },
+  {
+    "revision": "6226d72e409aeaa958218986e74ffc42",
+    "url": "/Booking-Movies-ReactJS/static/media/slide5.6226d72e.png"
+  },
+  {
+    "revision": "ed00aa57bce74267d8dff56b31036364",
+    "url": "/Booking-Movies-ReactJS/static/media/southpaw.ed00aa57.png"
+  },
+  {
+    "revision": "f507f9a4e83d3a884ee3bbacd7a613e7",
+    "url": "/Booking-Movies-ReactJS/static/media/specter.f507f9a4.png"
+  },
+  {
+    "revision": "c4670982530068bebfaa895049b4a685",
+    "url": "/Booking-Movies-ReactJS/static/media/techcombank.c4670982.png"
+  },
+  {
+    "revision": "3b8968aa0f2fdff5faf9ba991461835b",
+    "url": "/Booking-Movies-ReactJS/static/media/ted-2.3b8968aa.png"
+  },
+  {
+    "revision": "7654916e56c9e38c101fcc232830c7e7",
+    "url": "/Booking-Movies-ReactJS/static/media/the-longest-ride.7654916e.png"
+  },
+  {
+    "revision": "1290be5e87b8dba2dbce8a4a29f0cc93",
+    "url": "/Booking-Movies-ReactJS/static/media/the-walking-dead.1290be5e.png"
+  },
+  {
+    "revision": "06e345a4de40eea2cfa16a9637554fcf",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-ant-man.06e345a4.png"
+  },
+  {
+    "revision": "778688a113b32764ff25c764edad87ee",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-avengers-infinity-war.778688a1.png"
+  },
+  {
+    "revision": "10701cfcc649350cad9fe5d64252b2a2",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-batman-v-superman-dawn-of-justicess-doc.10701cfc.png"
+  },
+  {
+    "revision": "751cc3f3044cfe91fa3c10c56275f229",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-fantastic-four.751cc3f3.png"
+  },
+  {
+    "revision": "f6d21f00dbab33e2bef67c00b5182007",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-home.f6d21f00.png"
+  },
+  {
+    "revision": "9009b602d1703aa2d199048166d9689e",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-inside-out.9009b602.png"
+  },
+  {
+    "revision": "e49b0764a80bd2251f3c3f191c870a8d",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-jurassic-world.e49b0764.png"
+  },
+  {
+    "revision": "811ded3b7b8a9a26726f684edd590d6c",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-mad-max-fury-road.811ded3b.png"
+  },
+  {
+    "revision": "b31f7dcc89445374c4f2a700b17f697c",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-southpaw.b31f7dcc.png"
+  },
+  {
+    "revision": "4d94542163a981bbd93e229608489a64",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-specter.4d945421.png"
+  },
+  {
+    "revision": "61162acc16b5d9cf2a2d61c169fde023",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-ted-2.61162acc.png"
+  },
+  {
+    "revision": "7c3131057a21e02708473fa25ebe87e8",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-the-longest-ride.7c313105.png"
+  },
+  {
+    "revision": "6f2bdef3d5f5df270f725a7dbd8abfa1",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-the-walking-dead.6f2bdef3.png"
+  },
+  {
+    "revision": "6aca8d904aa512b6b16d65b0f1b6e9aa",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-trainwreck.6aca8d90.png"
+  },
+  {
+    "revision": "030059dc3a84a644c7c891c19a01e746",
+    "url": "/Booking-Movies-ReactJS/static/media/ticket-true-detective.030059dc.png"
+  },
+  {
+    "revision": "2d7a49b74c2db3aaf1dd8be5b058ae97",
+    "url": "/Booking-Movies-ReactJS/static/media/tpbank.2d7a49b7.png"
+  },
+  {
+    "revision": "8dd5991f38df39f6cc922290408ae080",
+    "url": "/Booking-Movies-ReactJS/static/media/trainwreck.8dd5991f.png"
+  },
+  {
+    "revision": "b923615c63bf918e233cd2dc5ec82630",
+    "url": "/Booking-Movies-ReactJS/static/media/true-detective.b923615c.png"
+  },
+  {
+    "revision": "5797db8accff21757c1941f7bb064a88",
+    "url": "/Booking-Movies-ReactJS/static/media/vib.5797db8a.png"
+  },
+  {
+    "revision": "48cb79d739b9a71836b72c81ab7727d3",
+    "url": "/Booking-Movies-ReactJS/static/media/vietabank.48cb79d7.png"
+  },
+  {
+    "revision": "abb51f4b2b4130f01edeeea1fecea0d6",
+    "url": "/Booking-Movies-ReactJS/static/media/vietcapitalbank.abb51f4b.png"
+  },
+  {
+    "revision": "1acd40e6312bcc4c95a6966048366dd8",
+    "url": "/Booking-Movies-ReactJS/static/media/vietinbank.1acd40e6.png"
+  },
+  {
+    "revision": "9741d54578eb9cb054ecf9c797745bf3",
+    "url": "/Booking-Movies-ReactJS/static/media/vrb.9741d545.png"
+  },
+  {
+    "revision": "92e6113f3c88bf5708d27d8a95f91500",
+    "url": "/Booking-Movies-ReactJS/static/media/zalopay.92e6113f.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Booking-Movies-ReactJS/precache-manifest.27fe6801f054c16db4435ac447f8153e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Booking-Movies-ReactJS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
